d461f258ad4275eda1c8c04b8394273d19e90e36
André Brandão <andrebrandao12@yahoo.com>
2024-04-24 20:45:57 -0300

    Projeto criado

---
+++ backend/src/main/java/com/ab/abcontato/AbcontatoApplication.java
@@ -0,0 +1,13 @@
+package com.ab.abcontato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcontatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcontatoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ab/abcontato/AbcontatoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ab.abcontato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcontatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
